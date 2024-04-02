de4ba1e0b1e038e97dcabab93df4e40319a4e094
josiasqr25 <162774317+josiasqr25@users.noreply.github.com>
2024-04-02 12:51:32 -0400

    Update ip-locate.py
    added main() function

--- ip-locate.py
+++ ip-locate.py
@@ -1,52 +1,58 @@
 import requests
 import sys
 
-while True:
-    ipAddr = input("IP Address: ")
-    info = input("[S]hort or [C]omplete information? ")
-    urlShort = "http://ip-api.com/json/" + ipAddr + "?fields=country,regioName,city,lat,lon,isp"
-    urlMore = "http://ip-api.com/json/" + ipAddr
+def main():
 
     while True:
-        if info.upper() == "S":
-            response = requests.get(urlShort)
-            print(response.text)
-        
-            while True:
-                option = input("[L]ocate another IP address, [W]rite to file, or [E]xit? ")
-                if option.upper() == "L":
-                    break
-                elif option.upper() == "W":
-                    with open(ipAddr + ".txt", "w") as file:
-                        file.write(response.text)
-                        print("Contents written to file.")
-                        continue
-                elif option.upper() == "E":
-                    sys.exit()
-                else:
-                    print("Invalid response.")
-                    continue
 
-        elif info.upper() == "C":
-            response = requests.get(urlMore)
-            print(response.text)
-            while True:
-                option = input("[L]ocate another IP address, [W]rite to file, or [E]xit? ")
-                if option.upper() == "L":
-                    break
-                elif option.upper() == "W":
-                    with open(ipAddr + ".txt", "w") as file:
-                        file.write(response.text)
-                        print("Contents written to file.")
-                        continue
-                elif option.upper() == "E":
-                    sys.exit()
-                else:
-                    print("Invalid response.")
-                    continue
+        ipAddr = input("IP Address: ")
+        info = input("[S]hort or [C]omplete information? ")
+        urlShort = "http://ip-api.com/json/" + ipAddr + "?fields=country,regioName,city,lat,lon,isp"
+        urlMore = "http://ip-api.com/json/" + ipAddr
 
-        else:
-            print("Invalid response.")
-            continue
-        
-        break
+        while True:
+            if info.upper() == "S":
+                response = requests.get(urlShort)
+                print(response.text)
+
+                while True:
+                    option = input("[L]ocate another IP address, [W]rite to file, or [E]xit? ")
+                    if option.upper() == "L":
+                        break
+                    elif option.upper() == "W":
+                        with open(ipAddr + ".txt", "w") as file:
+                            file.write(response.text)
+                            print("Contents written to file.")
+                            continue
+                    elif option.upper() == "E":
+                        sys.exit()
+                    else:
+                        print("Invalid response.")
+                        continue
+
+            elif info.upper() == "C":
+                response = requests.get(urlMore)
+                print(response.text)
+                while True:
+                    option = input("[L]ocate another IP address, [W]rite to file, or [E]xit? ")
+                    if option.upper() == "L":
+                        break
+                    elif option.upper() == "W":
+                        with open(ipAddr + ".txt", "w") as file:
+                            file.write(response.text)
+                            print("Contents written to file.")
+                            continue
+                    elif option.upper() == "E":
+                        sys.exit()
+                    else:
+                        print("Invalid response.")
+                        continue
+
+            else:
+                print("Invalid response.")
+                continue
+
+            break
+            
+if __name__ == "__main__":
+    main()
